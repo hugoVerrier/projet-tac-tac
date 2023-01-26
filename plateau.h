2fd1f88e8a333c21a82f8db9f690c2326a6d33c4
tomraviot <tomraviot@gmail.com>
2023-01-26 09:39:57 +0100

    plat

--- plateau.h
+++ plateau.h
@@ -5,6 +5,10 @@
 #ifndef PROJET_TAC_TAC_PLATEAU_H
 #define PROJET_TAC_TAC_PLATEAU_H
 
-void afficherPlateau();
+#include "tactac.h"
+void initialiserPlateau(Tuile plateau[12][26]);
+void afficherPlateau(Tuile plateau[12][26]);
+
+void aller(Tuile plateau[12][26]);
 
 #endif //PROJET_TAC_TAC_PLATEAU_H
